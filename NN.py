7c4d4912ed948485a5905a6e691e2718a349d8e1
cybertesla <vinayag388@gmail.com>
2025-04-15 00:57:36 +0530

    Made Activation functions global

--- NN.py
+++ NN.py
@@ -7,7 +7,13 @@ arr_y = np.array([[1], [0], [0], [1]])
 arr_w_01 = np.array([])
 # def forward():
     
-
+def sigmoid(self, x):
+    # The sigmoid activation function
+    return 1 / (1 + np.exp(-x))
+    
+def dsigmoid_dx(self, x):
+    # First derivative of the sigmoid activation function
+    return x * (1-x)
 class NN():
 
     # Specify Hyperparameters i.e. The Architecture of the Network.
@@ -26,21 +32,16 @@ class NN():
         self.b01 = np.random.uniform(size=(1,num_hidden)) # biases for hidden layer 
         self.b12 = np.random.uniform(size=(1,num_output)) # biases for output layer
 
-    def sigmoid(self, x):
-        # The sigmoid activation function
-
-        return 1 / (1 + np.exp(-x))
-    
-    def dsigmoid_dx(self, x):
-        # First derivative of the sigmoid activation function
-        return x * (1-x)
 
     def forward(self, X):
         # Forward Propagation through the network
+        # Implementing wX+b
         self.h1 = np.dot(X, self.weights_01) + self.b01 # operating from input layer to hidden layer
         a1 = sigmoid(self.h1) # applying activation function
         self.op = np.dot(self.h1, self.weights_12) + self.b12 # operating from hidden layer to output layer
-        a2 = sigmoid(self.op)
+        a2 = sigmoid(self.op) # Final output
+
+
 
     
 
